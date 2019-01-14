02dd3ea9b00c3b77fabf616431854482cb1448e1
vdepetigny <vdepetigny@yahoo.fr>
2019-01-14 13:30:51 +0100

    2 & 3 complet

--- lib/02_calculator.rb
+++ lib/02_calculator.rb
@@ -1,2 +1,33 @@
-def add
-	
+def add(x,y)
+	addresult = x + y
+	return addresult
+end
+
+
+def subtract(x,y)
+	subresult = x - y 
+	return subresult
+end
+
+def sum(array)
+	sumresult = array.sum
+	return  sumresult
+end
+
+
+def multiply(x,y)
+	multresult = x*y
+	return multresult
+end
+
+
+def power(x,y)
+	powresult = x**y
+	return powresult
+end
+
+
+def factorial(x)
+	x = (1..x).inject(:*) || 1
+	return x
+end
